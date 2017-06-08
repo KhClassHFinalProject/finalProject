0d1738f0bc300df85cbee530cd1b833c0d180423
largeKim <kdw4458@naver.com>
2017-06-08 15:38:19 +0900

    java

--- lee/src/main/java/ju/controller/IndexController.java
+++ lee/src/main/java/ju/controller/IndexController.java
@@ -1,19 +1,29 @@
 package ju.controller;
 
-import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
-    Logger log = Logger.getLogger(this.getClass());
 
 	
-	@RequestMapping(value="/index.ju")
-	public ModelAndView index(){
-		ModelAndView mav = new ModelAndView("index");
-		log.debug("인터셉터 테스트");
-		return mav;
+	@RequestMapping("/index.ju")
+	public String toIndex(){
+		
+		return "index";
+		
 	}
+	
+	
+	@RequestMapping("/myCard2.ju")
+	public String toMycard2(){
+		
+		return "member/myCard2";
+		
+	}
+	
+	
+	
+	
 }
